4ceaf27b9e7ce991d22f69ae69eec85275f0ff4d
shiv kumar <kumarshiv_sk@outlook.com>
2025-04-01 20:45:51 -0400

    Add project files.

---
+++ Week14bLoginCodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Week14bLoginCodeFirst.Models;
+
+namespace Week14bLoginCodeFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week14bLoginCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Week14bLoginCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
