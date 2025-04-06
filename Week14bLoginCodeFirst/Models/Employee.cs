97a39d9d439dde12697a549f50a87572392462ea
shiv kumar <kumarshiv_sk@outlook.com>
2025-04-06 09:27:23 -0400

    Special Built In Data Annotations on Form
    EMail Fields, Max Length
    
    https://dotnetfullstackdev.medium.com/new-data-annotations-in-net-8-ef5d61813596
    
    https://medium.com/@ravipatel.it/comprehensive-guide-to-data-annotations-in-net-8-a-beginner-friendly-approach-e6d6c8a8588f

--- Week14bLoginCodeFirst/Models/Employee.cs
+++ Week14bLoginCodeFirst/Models/Employee.cs
@@ -6,12 +6,20 @@ namespace Week14bLoginCodeFirst.Models
     {
         [Key]     // to make id primary key
         public int Id { get; set; }   // properties  - auto type
-        [Required(ErrorMessage = "Please enter the Email address")]
+
+        //[Required(ErrorMessage = "Please enter the Email address")]
+        //[MaxLength(3, ErrorMessage = "Please enter the Email address")]
+
+
+        [EmailAddress(ErrorMessage = "Please enter a valid Email address")]
         public required string Email { get; set; }
         [Required(ErrorMessage = "Please enter the Password")]
         public required string Password { get; set; }
         //[Required(ErrorMessage = "Please enter the First Name")]
+
+        //[MaxLength(3,ErrorMessage = "Please enter the FirstName")]
         public string? FirstName { get; set; }
+
 
         public string? LastName { get; set; }
 
